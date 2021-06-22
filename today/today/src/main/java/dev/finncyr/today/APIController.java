829ff7e904be63279a63f06f2b85cde466fbb680
Finn Cyriax <fcyriax@web.de>
2021-06-22 16:26:04 +0200

    fix MS4

--- today/today/src/main/java/dev/finncyr/today/APIController.java
+++ today/today/src/main/java/dev/finncyr/today/APIController.java
@@ -1,7 +1,15 @@
 package dev.finncyr.today;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +20,8 @@ import dev.finncyr.today.models.UserRepository;
 
 @RestController
 public class APIController {
+
+    private final String UPLOAD_DIR = "./src/main/resources/static/";
 
     @Autowired
     private UserRepository userRepository;
@@ -24,5 +34,11 @@ public class APIController {
     @GetMapping(value = "/api/posts/{id}", produces = "application/json")
     public Post getPostById(@PathVariable("id") Long id){
         return userRepository.findById(id).get();
+    }
+
+    @GetMapping(value = "/api/posts/{id}/picture", produces = "image/png")
+    public byte[] getPostPicture(@PathVariable("id") Long id) throws IOException{
+        String fileName = userRepository.findById(id).get().getFilename();
+        return Files.readAllBytes(Paths.get(UPLOAD_DIR + fileName));
     }
 }
